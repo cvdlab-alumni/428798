0d8e933b5fd44a859871a73923caa143b5b346fd
tilomelli <tilomelli@users.noreply.github.com>
2014-06-11 15:36:07 +0200

    added extractFacets function

--- OBJExporter/objexporter.py
+++ OBJExporter/objexporter.py
@@ -1,4 +1,17 @@
 
+''' Facets extraction of a block diagram '''
+def extractFacets(master, emptyChain=[]):
+	solidCV = [cell for k,cell in enumerate(master[1]) if not (k in emptyChain)]
+	exteriorCV =  [cell for k,cell in enumerate(master[1]) if k in emptyChain]
+	exteriorCV += exteriorCells(master)
+	CV = solidCV + exteriorCV
+	V = master[0]
+	FV = [f for f in larFacets((V,CV),3,len(exteriorCV))[1] if len(f) >= 4]
+	BF = boundaryCells(solidCV,FV)
+	boundaryFaces = [FV[face] for face in BF]
+	B_Rep = V,boundaryFaces
+	return B_Rep
+
 ''' Exports a model (V,FV) into an .obj format file at 'filePath' '''
 def objExporter((V,FV), filePath):
 	out_file = open(filePath,"w")
